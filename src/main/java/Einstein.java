8ce7b4d892e0cd2a53ad0497ed5ab9593af32b98
a'qil <131641361+qilstiano@users.noreply.github.com>
2025-01-24 01:38:38 +0800

    feat/delete
    - feat/delete
    - feat/help
    - styles/einstein-response
    - styles/responder

--- src/main/java/Einstein.java
+++ src/main/java/Einstein.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Task {
@@ -77,8 +78,7 @@ class EinsteinException extends Exception {
 public class Einstein {
     public String chatbotAscii;
     public String chatbotName = "Einstein";
-    private Task[] tasks = new Task[100]; // Array to store Task objects
-    private int taskCount = 0;
+    private ArrayList<Task> tasks = new ArrayList<>(); // Use ArrayList to store tasks
 
     // ANSI color codes for orange gradient
     private static final String[] ORANGE_GRADIENT = {
@@ -95,86 +95,112 @@ public class Einstein {
                         " .---.  __   _ .--.   .--. `| |-'.---.  __   _ .--.   \r\n" + //
                         "/ /__\\\\[  | [ `.-. | ( (`\\] | | / /__\\\\[  | [ `.-. |  \r\n" + //
                         "| \\__., | |  | | | |  `'.'. | |,| \\__., | |  | | | |  \r\n" + //
-                        " '.__.'[___][___||__][\\__) )\\__/ '.__.'[___][___||__] \r\n" + //
+                        " '.__.'[___][___||__][\\__) )\\__/ '.__.'[___][___||__]  v1.0\r\n" + //
                         "                                                     ";
     }
 
     public void greeting() {
         System.out.println("____________________________________________________________");
         System.out.println(getGradientText(chatbotAscii));
-        System.out.println("\nGuten tag, " + chatbotName + " here! What can I do for you?");
+        System.out.println(getGradientText("\nEinstein\nGuten tag, " + chatbotName + " here! What can I do for you?"));
         System.out.println("____________________________________________________________");
     }
 
     public void farewell() {
         System.out.println("____________________________________________________________");
-        System.out.println("\tBye, hope to see you again soon!");
+        System.out.println(getGradientText("Einstein\n\tBye, hope to see you again soon!"));
         System.out.println("____________________________________________________________");
     }
 
     public void addTask(Task task) {
-        if (taskCount < 100) {
-            tasks[taskCount] = task;
-            taskCount++;
-            System.out.println("____________________________________________________________");
-            System.out.println("Got it. I've added this task:");
-            System.out.println("  " + task);
-            System.out.println("Now you have " + taskCount + " tasks in the list.");
-            System.out.println("____________________________________________________________");
-        } else {
-            System.out.println("____________________________________________________________");
-            System.out.println("Sorry! As much as I'm a genius and REALLY smart, I can't store more than 100 tasks :(");
-            System.out.println("____________________________________________________________");
-        }
+        tasks.add(task);
+        System.out.println("____________________________________________________________");
+        System.out.println(getGradientText("Einstein\nGot it. I've added this task:"));
+        System.out.println("  " + task);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
     }
 
     public void listTasks() {
         System.out.println("____________________________________________________________");
+        System.out.println(getGradientText("Einstein\nHere are the tasks in your list:"));
 
-        if (taskCount == 0) {
+        if (tasks.isEmpty()) {
             System.out.println("Hmmm, didn't find any tasks. Add some tasks!");
         } else {
-            System.out.println("Here are the tasks in your list:");
-            for (int i = 0; i < taskCount; i++) {
-                System.out.println((i + 1) + "." + tasks[i]);
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + "." + tasks.get(i));
             }
         }
 
         System.out.println("____________________________________________________________");
     }
 
-    public void markTaskAsDone(int taskIndex) {
-        if (taskIndex >= 0 && taskIndex < taskCount) {
-            tasks[taskIndex].markAsDone();
+    public void markTaskAsDone(int taskIndex) throws EinsteinException {
+        if (taskIndex >= 0 && taskIndex < tasks.size()) {
+            tasks.get(taskIndex).markAsDone();
             System.out.println("____________________________________________________________");
-            System.out.println("Nice! I've marked this task as done:");
-            System.out.println("  " + tasks[taskIndex]);
+            System.out.println(getGradientText("Einstein\nNice! I've marked this task as done:"));
+            System.out.println("  " + tasks.get(taskIndex));
             System.out.println("____________________________________________________________");
         } else {
-            System.out.println("____________________________________________________________");
-            System.out.println("Invalid task number!");
-            System.out.println("____________________________________________________________");
+            throw new EinsteinException("Invalid task number! Please give me something valid!");
         }
     }
 
-    public void markTaskAsNotDone(int taskIndex) {
-        if (taskIndex >= 0 && taskIndex < taskCount) {
-            tasks[taskIndex].markAsNotDone();
+    public void markTaskAsNotDone(int taskIndex) throws EinsteinException {
+        if (taskIndex >= 0 && taskIndex < tasks.size()) {
+            tasks.get(taskIndex).markAsNotDone();
             System.out.println("____________________________________________________________");
-            System.out.println("Awesome, I've marked this task as not done yet, remember to get to it:");
-            System.out.println("  " + tasks[taskIndex]);
+            System.out.println(getGradientText("Einstein\nAwesome, I've marked this task as not done yet, remember to get to it:"));
+            System.out.println("  " + tasks.get(taskIndex));
             System.out.println("____________________________________________________________");
         } else {
-            System.out.println("____________________________________________________________");
-            System.out.println("Invalid task number!");
-            System.out.println("____________________________________________________________");
+            throw new EinsteinException("Invalid task number! Please give me something valid!");
         }
+    }
+
+    public void deleteTask(int taskIndex) throws EinsteinException {
+        if (taskIndex >= 0 && taskIndex < tasks.size()) {
+            Task removedTask = tasks.remove(taskIndex);
+            System.out.println("____________________________________________________________");
+            System.out.println(getGradientText("Einstein\nNoted. I've atomized this task:"));
+            System.out.println("  " + removedTask);
+            System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            System.out.println("____________________________________________________________");
+        } else {
+            throw new EinsteinException("Invalid task number! Please give me something valid!");
+        }
+    }
+
+    public void displayHelp() {
+        System.out.println("____________________________________________________________");
+        System.out.println(getGradientText("Einstein\nHere are the commands I understand:"));
+        System.out.println("1. todo <description> - Add a todo task.");
+        System.out.println("   Example: todo read book");
+        System.out.println("2. deadline <description> /by <date> - Add a deadline task.");
+        System.out.println("   Example: deadline return book /by Sunday");
+        System.out.println("3. event <description> /from <start> /to <end> - Add an event task.");
+        System.out.println("   Example: event project meeting /from Mon 2pm /to 4pm");
+        System.out.println("4. list - List all tasks.");
+        System.out.println("   Example: list");
+        System.out.println("5. mark <task number> - Mark a task as done.");
+        System.out.println("   Example: mark 1");
+        System.out.println("6. unmark <task number> - Mark a task as not done.");
+        System.out.println("   Example: unmark 1");
+        System.out.println("7. delete <task number> - Delete a task.");
+        System.out.println("   Example: delete 1");
+        System.out.println("8. help - Display this help message.");
+        System.out.println("   Example: help");
+        System.out.println("9. bye - Exit the program.");
+        System.out.println("   Example: bye");
+        System.out.println("____________________________________________________________");
     }
 
     public void processCommands() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("> ");
+            System.out.print("User\n> ");
             String userInput = scanner.nextLine().trim();
 
             try {
@@ -200,17 +226,23 @@ public class Einstein {
                 } else if (userInput.startsWith("event ")) {
                     handleEventCommand(userInput);
 
+                } else if (userInput.startsWith("delete ")) {
+                    handleDeleteCommand(userInput);
+
+                } else if (userInput.equalsIgnoreCase("help")) {
+                    displayHelp();
+
                 } else {
                     throw new EinsteinException("ARGH! I do not understand you, which is weird, \nbecause I usually understand most things. Invalid command!");
                 }
 
             } catch (EinsteinException e) {
                 System.out.println("____________________________________________________________");
-                System.out.println(e.getMessage());
+                System.out.println(getGradientText("Einstein\n" + e.getMessage()));
                 System.out.println("____________________________________________________________");
             } catch (NumberFormatException e) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Invalid task number! Please give me something valid!");
+                System.out.println(getGradientText("Einstein\nInvalid task number! Please give me something valid!"));
                 System.out.println("____________________________________________________________");
             }
         }
@@ -220,9 +252,6 @@ public class Einstein {
     private void handleMarkCommand(String userInput) throws EinsteinException {
         try {
             int taskIndex = Integer.parseInt(userInput.substring(5).trim()) - 1;
-            if (taskIndex < 0 || taskIndex >= taskCount) {
-                throw new EinsteinException("Invalid task number! Please give me something valid!");
-            }
             markTaskAsDone(taskIndex);
         } catch (NumberFormatException e) {
             throw new EinsteinException("Invalid task number! Please give me something valid!");
@@ -232,9 +261,6 @@ public class Einstein {
     private void handleUnmarkCommand(String userInput) throws EinsteinException {
         try {
             int taskIndex = Integer.parseInt(userInput.substring(7).trim()) - 1;
-            if (taskIndex < 0 || taskIndex >= taskCount) {
-                throw new EinsteinException("Invalid task number! Please give me something valid!");
-            }
             markTaskAsNotDone(taskIndex);
         } catch (NumberFormatException e) {
             throw new EinsteinException("Invalid task number! Please give me something valid!");
@@ -268,6 +294,22 @@ public class Einstein {
         String from = parts[1].trim();
         String to = parts[2].trim();
         addTask(new Event(description, from, to));
+    }
+
+    public void handleDeleteCommand(String userInput) throws EinsteinException {
+        try {
+            if (tasks.size() == 0) {
+                throw new EinsteinException("We don't have any tasks yet, my dear friend. Add some first!");
+            }
+            String taskNumberStr = userInput.substring(7).trim();
+            if (taskNumberStr.isEmpty()) {
+                throw new EinsteinException("Nein! You must provide a task number to delete. \nUse: delete <task number>");
+            }
+            int taskIndex = Integer.parseInt(taskNumberStr) - 1;
+            deleteTask(taskIndex);
+        } catch (NumberFormatException e) {
+            throw new EinsteinException("Invalid task number! Please give me something valid!");
+        }
     }
 
     // helper method to apply gradient colour to text (only for ASCII art) - created by DeepSeek
