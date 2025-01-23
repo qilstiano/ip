266cad8cdbd3f8fa3df112f8d19fec63c7e1e740
a'qil <131641361+qilstiano@users.noreply.github.com>
2025-01-24 00:50:14 +0800

    feat/tasks
    feat/todo
    feat/event
    feat/deadline

--- src/main/java/Einstein.java
+++ src/main/java/Einstein.java
@@ -10,7 +10,7 @@ class Task {
     }
 
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); //mark done task with X
+        return (isDone ? "X" : " "); // mark done task with X
     }
 
     public void markAsDone() {
@@ -24,6 +24,47 @@ class Task {
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
+    }
+}
+
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String from;
+    protected String to;
+
+    public Event(String description, String from, String to) {
+        super(description);
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
 
@@ -43,7 +84,7 @@ public class Einstein {
     private static final String RESET = "\u001B[0m"; // Reset color
 
     public Einstein() {
-        chatbotAscii =  "       _                   _           _            \r\n" + //
+        chatbotAscii =  "         _                   _           _            \r\n" + //
                         "        (_)                 / |_        (_)           \r\n" + //
                         " .---.  __   _ .--.   .--. `| |-'.---.  __   _ .--.   \r\n" + //
                         "/ /__\\\\[  | [ `.-. | ( (`\\] | | / /__\\\\[  | [ `.-. |  \r\n" + //
@@ -53,108 +94,142 @@ public class Einstein {
     }
 
     public void greeting() {
-        System.out.println(("____________________________________________________________"));
-        System.out.println(getGradientText("\s\s" + chatbotAscii));
-        System.out.println(("\nGuten tag, " + chatbotName + " here! What can I do for you?"));
-        System.out.println(("____________________________________________________________"));
+        System.out.println("____________________________________________________________");
+        System.out.println(getGradientText(chatbotAscii));
+        System.out.println("\nGuten tag, " + chatbotName + " here! What can I do for you?");
+        System.out.println("____________________________________________________________");
     }
 
     public void farewell() {
-        System.out.println(("____________________________________________________________"));
-        System.out.println(("\tBye. Hope to see you again soon!"));
-        System.out.println(("____________________________________________________________"));
+        System.out.println("____________________________________________________________");
+        System.out.println("\tBye, hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 
-    public void addTask(String taskDescription) {
+    public void addTask(Task task) {
         if (taskCount < 100) {
-            tasks[taskCount] = new Task(taskDescription);
+            tasks[taskCount] = task;
             taskCount++;
-            System.out.println(("____________________________________________________________"));
-            System.out.println(("\nadded: " + taskDescription));
-            System.out.println(("____________________________________________________________"));
+            System.out.println("____________________________________________________________");
+            System.out.println("Got it. I've added this task:");
+            System.out.println("  " + task);
+            System.out.println("Now you have " + taskCount + " tasks in the list.");
+            System.out.println("____________________________________________________________");
         } else {
-            System.out.println(("____________________________________________________________"));
-            System.out.println(("Sorry! As much as I'm a genius and REALLY smart, I can't store more than 100 tasks :("));
-            System.out.println(("____________________________________________________________"));
+            System.out.println("____________________________________________________________");
+            System.out.println("Sorry! As much as I'm a genius and REALLY smart, I can't store more than 100 tasks :(");
+            System.out.println("____________________________________________________________");
         }
     }
 
     public void listTasks() {
-        System.out.println(("____________________________________________________________"));
-        System.out.println(("Here are the tasks in your list:"));
+        System.out.println("____________________________________________________________");
 
         if (taskCount == 0) {
-            System.out.println(("Hmmm, didn't find any tasks. Add some tasks!"));
+            System.out.println("Hmmm, didn't find any tasks. Add some tasks!");
         } else {
+            System.out.println("Here are the tasks in your list:");
             for (int i = 0; i < taskCount; i++) {
-                System.out.println(((i + 1) + "." + tasks[i]));
+                System.out.println((i + 1) + "." + tasks[i]);
             }
         }
 
-        System.out.println(("____________________________________________________________"));
+        System.out.println("____________________________________________________________");
     }
 
     public void markTaskAsDone(int taskIndex) {
         if (taskIndex >= 0 && taskIndex < taskCount) {
             tasks[taskIndex].markAsDone();
-            System.out.println(("____________________________________________________________"));
-            System.out.println(("Nice! I've marked this task as done:"));
-            System.out.println(("  " + tasks[taskIndex]));
-            System.out.println(("____________________________________________________________"));
+            System.out.println("____________________________________________________________");
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + tasks[taskIndex]);
+            System.out.println("____________________________________________________________");
         } else {
-            System.out.println(("____________________________________________________________"));
-            System.out.println(("Invalid task number!"));
-            System.out.println(("____________________________________________________________"));
+            System.out.println("____________________________________________________________");
+            System.out.println("Invalid task number!");
+            System.out.println("____________________________________________________________");
         }
     }
 
     public void markTaskAsNotDone(int taskIndex) {
         if (taskIndex >= 0 && taskIndex < taskCount) {
             tasks[taskIndex].markAsNotDone();
-            System.out.println(("____________________________________________________________"));
-            System.out.println(("OK, I've marked this task as not done yet:"));
-            System.out.println(("  " + tasks[taskIndex]));
-            System.out.println(("____________________________________________________________"));
+            System.out.println("____________________________________________________________");
+            System.out.println("Awesome, I've marked this task as not done yet, remember to get to it:");
+            System.out.println("  " + tasks[taskIndex]);
+            System.out.println("____________________________________________________________");
         } else {
-            System.out.println(("____________________________________________________________"));
-            System.out.println(("Invalid task number!"));
-            System.out.println(("____________________________________________________________"));
+            System.out.println("____________________________________________________________");
+            System.out.println("Invalid task number!");
+            System.out.println("____________________________________________________________");
         }
     }
 
     public void processCommands() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print(("> "));
+            System.out.print("> ");
             String userInput = scanner.nextLine().trim();
 
             if (userInput.equalsIgnoreCase("bye")) {
                 farewell();
                 break;
+            
             } else if (userInput.equalsIgnoreCase("list")) {
                 listTasks();
+            
             } else if (userInput.startsWith("mark ")) {
                 try {
                     int taskIndex = Integer.parseInt(userInput.substring(5).trim()) - 1;
                     markTaskAsDone(taskIndex);
                 } catch (NumberFormatException e) {
-                    System.out.println(("Invalid task number!"));
+                    System.out.println("Invalid task number!");
                 }
+
             } else if (userInput.startsWith("unmark ")) {
                 try {
                     int taskIndex = Integer.parseInt(userInput.substring(7).trim()) - 1;
                     markTaskAsNotDone(taskIndex);
                 } catch (NumberFormatException e) {
-                    System.out.println(("Invalid task number!"));
+                    System.out.println("Invalid task number!");
                 }
+
+            } else if (userInput.startsWith("todo ")) {
+                String description = userInput.substring(5).trim();
+                addTask(new Todo(description));
+
+            } else if (userInput.startsWith("deadline ")) {
+                String[] parts = userInput.substring(9).split("/by", 2);
+                
+                if (parts.length == 2) {
+                    String description = parts[0].trim();
+                    String by = parts[1].trim();
+                    addTask(new Deadline(description, by));
+                
+                } else {
+                    System.out.println("Invalid deadline format! Use: deadline <description> /by <date>");
+                }
+            
+            } else if (userInput.startsWith("event ")) {
+                String[] parts = userInput.substring(6).split("/from|/to", 3);
+                
+                if (parts.length == 3) {
+                    String description = parts[0].trim();
+                    String from = parts[1].trim();
+                    String to = parts[2].trim();
+                    addTask(new Event(description, from, to));
+                } else {
+                    System.out.println("Invalid event format! Use: event <description> /from <start> /to <end>");
+                }
+            
             } else {
-                addTask(userInput);
+                System.out.println("Invalid command!");
             }
         }
         scanner.close();
     }
 
-    // Helper method to apply gradient color to text
+    // helper method to apply gradient colour to text (only for ASCII art) - created by DeepSeek
     private String getGradientText(String text) {
         StringBuilder gradientText = new StringBuilder();
         int length = text.length();
